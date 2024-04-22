65fd894e9b49565d5f82a6bee8ec499c56344dc8
bulatsabirow <bulatsabirow@gmail.com>
2024-04-22 21:35:15 +0300

    + dockerize application

--- referral_system/settings.py
+++ referral_system/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r1mq5tuf&hr!gf(hs-!(ula=qg%rmvf-2b7-99h#exn3iv5=h+"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-r1mq5tuf&hr!gf(hs-!(ula=qg%rmvf-2b7-99h#exn3iv5=h+"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
